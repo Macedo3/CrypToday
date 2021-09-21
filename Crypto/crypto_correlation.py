6e5f777bc52270fad68e9b90d19cab39e37462b0
Gabriel de Macedo Santos <82056067+Macedo3@users.noreply.github.com>
2021-09-21 18:32:00 -0300

    Add files via upload

---
+++ Crypto/crypto_correlation.py
@@ -0,0 +1,50 @@
+import pandas_datareader as web
+import mplfinance as mpf
+import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## The currency who we gonn' convert
+currency = "USD"
+metric = "Close"
+
+## When start to now
+start = dt.datetime(2018,1,1)
+end = dt.datetime.now()
+
+## The cryptos 
+crypto = ['BTC', 'ETH', 'LTC', 'XRP', 'DASH', 'SC', '$SCCP']
+colnames = []
+
+## Program start
+first = True
+
+for ticker in crypto:
+    data = web.DataReader(f"{ticker}-{currency}", "yahoo", start, end)
+    if first:
+        combined = data[[metric]].copy()
+        colnames.append(ticker)
+        combined.columns = colnames
+        first = False
+    else:
+        combined = combined.join(data[metric])
+        colnames.append(ticker)
+        combined.columns = colnames
+
+plt.yscale('log') # first show linear
+
+for ticker in crypto:
+    plt.plot(combined[ticker], label=ticker)
+
+plt.legend(loc="upper right")
+
+plt.show()
+
+# # Correlation Heat Map
+
+print(combined)
+
+combined = combined.pct_change().corr(method='pearson')
+
+sns.heatmap(combined, annot=True, cmap="coolwarm")
+plt.show()
